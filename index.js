f4613c6dd223d944d6f026e53c2ee9f028b4833d
kylaputri <78312636+kylaputri@users.noreply.github.com>
2021-02-21 20:08:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -304,7 +304,6 @@ client.on('group-participants-update', async (anu) => {
 			if (mek.key.fromMe) return
 			global.blocked
 			const content = JSON.stringify(mek.message)
-			const from = mek.key.remoteJid
 			const type = Object.keys(mek.message)[0]
 			const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
 			const time = moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')
